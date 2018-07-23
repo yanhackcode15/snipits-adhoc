806b2279055b9d105d2703ffb398a24444a45a2a
yanhackcode15 <iamyanhu@gmail.com>
2018-07-23 16:05:51 -0700

    code review comments addressed

--- src/index.js
+++ src/index.js
@@ -6,20 +6,11 @@ module.exports = (fromDate, toDate) => {
 	return Promise.all([hoursCalc(fromDate, toDate), productivityCalc(fromDate, toDate)])
 		.then(dataArry=>{
 			var hoursData = dataArry[0];
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> using mongo db as cache and fetch from portal if db record not exist't
-=======
->>>>>>> code review comments addressed
 			var prodData = dataArry[1];
 			let allStats = {};
 			for (let key in hoursData) {
 				allStats[key] = Object.assign(hoursData[key], prodData[key]);
 			}
 			return allStats;
-<<<<<<< HEAD
 		});
-=======
->>>>>>> using mongo db as cache and fetch from portal if db record not exist't
 }
